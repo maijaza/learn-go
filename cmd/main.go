550c3ee08bb4c6f4a45e91b4c9937d9a6e41befc
maijaza <maijaza0@hotmail.com>
2020-02-20 10:10:52 +0700

    initial file

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"learn-go/internal/ch01"
+)
+
+func main() {
+	fmt.Println("Hello")
+	ch01.Print()
+}
---
+++ internal/ch01/ch01.go
@@ -0,0 +1,7 @@
+package ch01
+
+import "fmt"
+
+func Print() {
+	fmt.Print("01")
+}
